ccc38811797e46b4c42af2517a797949b3a1aa25
IbrahemAtef <bulbul.20001@gmail.com>
2020-11-10 16:28:40 +0200

    reset

--- db/models/post.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const PostSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  createdAt: { type: Date, required: true },
-});
-
-module.exports = mongoose.model('Posts', PostSchema);
