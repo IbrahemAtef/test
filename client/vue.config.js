5b90c51ec4c810f48fe884ca086d418d4355df12
IbrahemAtef <bulbul.20001@gmail.com>
2020-11-10 16:11:32 +0200

    ..

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    outputDir: path.resolve(__dirname, '../'),
+    outputDir: path.resolve(__dirname, '../test'),
     devServer: {
         proxy: {
             '/api': {
